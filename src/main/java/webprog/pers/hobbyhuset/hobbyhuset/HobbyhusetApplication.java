430e752c8872cd5b91c2aa65256f6b392f59d94c
JorBernt <berntsen.jorgen@gmail.com>
2021-04-09 08:56:30 +0200

    Initial commit

---
+++ src/main/java/webprog/pers/hobbyhuset/hobbyhuset/HobbyhusetApplication.java
@@ -0,0 +1,13 @@
+package webprog.pers.hobbyhuset.hobbyhuset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbyhusetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HobbyhusetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/webprog/pers/hobbyhuset/hobbyhuset/HobbyhusetApplicationTests.java
@@ -0,0 +1,13 @@
+package webprog.pers.hobbyhuset.hobbyhuset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyhusetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
